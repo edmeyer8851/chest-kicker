41435bf96aacb8c4d0e10c6328727c1ac6578dde
Ed_Meyer <edmeyer8851@gmail.com>
2025-08-24 15:49:28 -0400

    refactoring, remove config

--- src/main/java/com/example/ChestKickerPlugin.java
+++ src/main/java/com/example/ChestKickerPlugin.java
@@ -1,13 +1,9 @@
 package com.example;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -16,13 +12,10 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ChestKickerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
-
-	@Inject
-	private ExampleConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -39,15 +32,6 @@ public class ExamplePlugin extends Plugin
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
 
-	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ExampleConfig.class);
 	}
 }
